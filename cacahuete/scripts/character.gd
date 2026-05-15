51743ceba51517b0cbec718120e3719ae62a1022
Arnaud Elison <aelison@c1r7s10.42antananarivo.mg>
2026-05-15 15:29:07 +0300

    feat: starting player movements
    Testing to move player in all direction

---
+++ cacahuete/scripts/character.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 7
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+# How far the player can jump
+@export var jump = 2
+
+var target_velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	#if is_on_floor() and Input.is_action_just_pressed("jump"):
+		#direction.y += jump
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	#target_velocity.y = direction.y
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()

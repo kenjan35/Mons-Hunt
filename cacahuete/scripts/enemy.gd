92b658af2b5ca95f3a3a1190f9a31dcb7e2d286f
aelison <aelison@student.42antananarivo.mg>
2026-05-21 15:55:20 +0300

    update: enemy gravity and speed to reach the player

--- cacahuete/scripts/enemy.gd
+++ cacahuete/scripts/enemy.gd
@@ -1,9 +1,8 @@
 extends CharacterBody3D
 
-const SPEED = 5
+const SPEED = 8.0
 
 @export var player_path: NodePath
-
 @onready var agent = $NavigationAgent3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,18 +10,22 @@ var player = null
 
 func _ready() -> void:
 	player = get_node(player_path)
-	
-func _physics_process(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
 	
 	agent.set_target_position(player.global_position)
 	if agent.is_target_reachable():
 		var next_path = agent.get_next_path_position()
-		velocity = (next_path - global_position).normalized() * SPEED 
+		var move_dir = (next_path - global_position).normalized() * SPEED
+		velocity.x = move_dir.x
+		velocity.z = move_dir.z
 	else:
-		velocity = Vector3.ZERO
-	apply_gravity(_delta)
+		velocity.x = 0
+		velocity.z = 0
+	
 	move_and_slide()
 
-func apply_gravity(_delta):
+func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y -= gravity
+		velocity.y -= gravity * delta
